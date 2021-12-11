1e8534459cc807318ccaf712a6edd242c16e99c5
FaxriyorUmarxonov <faxriyorumarxonov@gmail.com>
2021-12-11 21:17:26 +0500

    imageDostup

--- src/main/java/com/example/employeecontrol/config/SecurityConfig.java
+++ src/main/java/com/example/employeecontrol/config/SecurityConfig.java
@@ -38,7 +38,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http
                 .csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/api/login","/manag/countfilenumber","/manag/getmanager","/manag/viewattach").permitAll()
+                .antMatchers("/api/login","/manag/countfilenumber","/manag/getmanager","/manag/viewattach","/imagelocation/").permitAll()
                 .anyRequest().authenticated();
         http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
